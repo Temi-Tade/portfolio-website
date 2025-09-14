54fdc9cc690960e0f883f2a9b133ecaac0000f4f
Temi-Tade <akintadetemi19@gmail.com>
2025-09-14 07:12:09 +0100

    set up config for gh pages deployment, set new images for favicon

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,19 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter({
+			pages: 'dist',
+			assets: 'dist',
+			fallback: '/src/app.html',
+			precompress: false,
+			strict: true
+		}),
+
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/portfolio-website' : ''
+		}
+	}
 };
 
 export default config;
